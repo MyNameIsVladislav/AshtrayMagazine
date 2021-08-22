fa8c60bbb746dd9232fbebfbdd0167ac5461c85c
VladBell <v.kolokolchikoff@yandex.ru>
2021-08-22 22:35:48 +0300

    feat(CN-33): добавил логгирование в проект

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,48 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        "rich": {"datefmt": "[%X]"},
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'formatter': 'verbose',
+            'filename': 'error.log'
+        },
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+
+        'console': {
+            'level': 'DEBUG',
+            'class': 'rich.logging.RichHandler',
+            'formatter': 'rich'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['console', 'file'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
