7cc236bd69f7a504d67687f13c27f0a8d5133662
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-13 17:47:59 +0300

    fix: fixed files included in review

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Register your models here.
-from .models import CustomUser, UserProfile
+from .models import User, UserProfile
 
-admin.site.register(CustomUser)
+admin.site.register(User)
 admin.site.register(UserProfile)
--- authapp/models.py
+++ authapp/models.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import timedelta
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -6,13 +6,14 @@ from django.utils.translation import ugettext as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.validators import RegexValidator
+from django.utils.timezone import now
 
 from .managers import CustomUserManager
 
 
 # Create your models here.
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     birthday = models.DateField(_('birthday'))
@@ -21,6 +22,20 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    activation_key = models.CharField(
+        max_length=128,
+        blank=True
+    )
+    activation_key_expires = models.DateTimeField(
+        default=(now() + timedelta(hours=48))
+    )
+
+    def is_activation_key_expired(self):
+        if now() <= self.activation_key_expires:
+            return False
+        else:
+            return True
 
     def __str__(self):
         return self.email
@@ -51,7 +66,7 @@ class UserProfile(models.Model):
         (EN, 'EN'),
     )
 
-    user = models.OneToOneField(CustomUser, unique=True, null=False,
+    user = models.OneToOneField(User, unique=True, null=False,
                                 db_index=True, on_delete=models.CASCADE)
     gender = models.CharField(verbose_name=_('gender'), max_length=10, choices=GENDER_CHOICES, blank=True)
     avatar = models.ImageField(verbose_name=_('avatar'), upload_to=user_directory_path, blank=True)
@@ -63,19 +78,19 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'Профиль пользователя: {self.user.email} - {self.user.id}'
 
-    @receiver(post_save, sender=CustomUser)
+    @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             UserProfile.objects.create(user=instance)
 
-    @receiver(post_save, sender=CustomUser)
+    @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.userprofile.save()
 
     @property
     def age(self):
         if self.user.birthday:
-            today = date.today()
+            today = now()
             return today.year - self.user.birthday.year - (
                     (today.month, today.day) < (self.user.birthday.month, self.user.birthday.day))
         return 0
