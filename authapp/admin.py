f785908664adcbddec92f5e3ba3d06aefbf7f5cb
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-16 21:53:34 +0300

    feat: added AdminModels in Authapp

--- authapp/admin.py
+++ authapp/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import User, UserProfile
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'first_name', 'last_name',
+                    'is_active', 'is_staff', 'date_joined']
+    list_display_links = ('id', 'email')
+    list_filter = ['first_name', 'last_name', 'email']
+    search_fields = ['first_name', 'last_name', 'email']
+    date_hierarchy = 'date_joined'
+    ordering = ['first_name', 'last_name', 'is_active']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'gender', 'region', 'age', 'phone')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, ProfileAdmin)
