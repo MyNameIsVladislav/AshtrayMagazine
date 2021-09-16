b71307a0677ba6baea52b80671b8c9d456111c15
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-16 22:02:53 +0300

    fix: fixed order app and deleted bascket app

--- orderapp/models.py
+++ orderapp/models.py
@@ -1,75 +1,44 @@
 from django.db import models
-from django.conf import settings
 
 from shopapp.models import Product
+from core.settings import AUTH_USER_MODEL
 
 
 class Order(models.Model):
-    FORMING = 'FM'
-    SENT_TO_PROCEED = 'STP'
-    PROCEEDED = 'PRD'
-    PAID = 'PD'
-    READY = 'RDY'
-    CANCEL = 'CNC'
-
-    ORDER_STATUS_CHOICES = (
-        (FORMING, 'формируется'),
-        (SENT_TO_PROCEED, 'отправлен в обработку'),
-        (PAID, 'оплачен'),
-        (PROCEEDED, 'обрабатывается'),
-        (READY, 'готов к выдаче'),
-        (CANCEL, 'отменен'),
-    )
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-    created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
-    updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
-    status = models.CharField(verbose_name='статус',
-                              max_length=3,
-                              choices=ORDER_STATUS_CHOICES,
-                              default=FORMING)
-    is_active = models.BooleanField(verbose_name='активен', default=True)
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='user')
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('-created',)
-        verbose_name = 'заказ'
-        verbose_name_plural = 'заказы'
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
     def __str__(self):
-        return 'Текущий заказ: {}'.format(self.id)
-
-    def get_total_quantity(self):
-        items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.quantity, items)))
-
-    def get_product_type_quantity(self):
-        items = self.orderitems.select_related()
-        return len(items)
+        return 'Order {}'.format(self.id)
 
     def get_total_cost(self):
-        items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.quantity * x.product.price, items)))
-
-        # переопределяем метод, удаляющий объект
-
-    def delete(self):
-        for item in self.orderitems.select_related():
-            item.product.quantity += item.quantity
-            item.product.save()
-
-        self.is_active = False
-        self.save()
+        return sum(item.get_cost() for item in self.items.all())
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
-                              related_name="orderitems",
-                              on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,
-                                verbose_name='продукт',
-                                on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name='количество',
-                                           default=0)
+                              on_delete=models.CASCADE,
+                              related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='order_items')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
 
-    def get_product_cost(self):
-        return self.product.price * self.quantity
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
