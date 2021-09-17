899f64d04a7a0ffa4b5c3596d100c3eb4c461acc
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-17 10:42:20 +0300

    fix: fixed models in ArticleAPP

--- articleapp/models.py
+++ articleapp/models.py
@@ -22,19 +22,10 @@ class Author(models.Model):
 
 class Genres(models.Model):
     name = models.CharField(verbose_name=_('genre'), max_length=16)
+    slug = models.SlugField(verbose_name=_('genre'))
 
     def __str__(self):
         return f'{self.name} - {self.pk}'
-
-
-class ArticleDataModel(models.Model):
-    tag = models.CharField(max_length=20, verbose_name=_('tag'))
-    text = models.TextField(verbose_name=_('text'), null=True)
-    img_mini = models.ImageField(upload_to=choose_path, verbose_name=_('image mini'))
-    img_main = models.ImageField(upload_to=choose_path, verbose_name=_('image main'))
-
-    def __str__(self):
-        return f'article file {self.tag} - {self.pk}'
 
 
 #
@@ -60,12 +51,14 @@ class ArticleModel(models.Model):
 
     title = models.CharField(max_length=70, verbose_name=_('title'), default=_('Not found'))
     short_describe = models.TextField(verbose_name=_('short describe'), max_length=150, default=_('Not Found'))
-    articles_data_id = models.ForeignKey(ArticleDataModel, on_delete=models.SET_NULL,
-                                         verbose_name=_('article'), null=True, related_name='article_file')
     owner_id = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True,
                                  verbose_name=_('owner'), related_name='owner')
     genres_id = models.ManyToManyField(Genres, related_name='genres', related_query_name='genre')
     slug = models.SlugField(max_length=70, unique_for_date='publish')
+    text = models.TextField(verbose_name=_('text'), null=True)
+    img_mini = models.ImageField(upload_to=choose_path, verbose_name=_('image mini'))
+    img_main = models.ImageField(upload_to=choose_path, verbose_name=_('image main'))
+
     publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
