6917d8597ed9016a35ee64dddbdb0b8b3b910283
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-18 19:26:01 +0300

    feat: added views and templates for ShopApp

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import ugettext as _
 
 
@@ -13,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:categories', kwargs={'slug_cat': self.slug})
 
 
 class Product(models.Model):
@@ -33,3 +37,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product', kwargs={'slug_cat': self.category.slug, 'slug': self.slug})
+
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from shopapp.models import Product, Category
+
+
+class ShowProduct(ListView):
+    model = Product
+    template_name = 'shop/catalog.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ShowProduct, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class CategoryProduct(ListView):
+    model = Product
+    template_name = 'shop/catalog.html'
+    context_object_name = 'product'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug_cat'], available=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CategoryProduct, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'shop/product.html'
+    slug_url_kwarg = 'slug'
+    context_object_name = 'product'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if get_object_or_404(Category, slug=kwargs['slug_cat']):
+            return super(ProductDetail, self).get(request, *args, **kwargs)
+        else:
+            raise Http404
