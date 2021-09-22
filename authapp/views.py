a66d5d4c15422e2a068adb2b2db304e2f63a328e
VladBell <v.kolokolchikoff@yandex.ru>
2021-09-22 11:32:01 +0300

    fix: fixed message in send_mail function (AuthApp)

--- authapp/views.py
+++ authapp/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.views import (
     PasswordResetView,
     PasswordResetConfirmView,
 )
+from django.utils.safestring import mark_safe
 
 from core import settings
 from .forms import *
@@ -23,16 +24,18 @@ def send_verify_mail(user):
     verify_link = reverse(
         'auth:verify',
         args=[user.email, user.activation_key])
+    key_activation = mark_safe(f'<a href="{settings.DOMAIN_NAME}{verify_link}"> Активировать </a>')
     title = 'Подтверждение учетной записи'
     message = f'Для подтверждения учетной записи {user.email}' \
-              f'пройдите по ссылке: <a href="{settings.DOMAIN_NAME}{verify_link}"> Активировать </a>'
+              f' пройдите по ссылке: {key_activation}'
 
     return send_mail(
         title,
         message,
         settings.EMAIL_HOST_USER,
         [user.email],
-        fail_silently=False
+        fail_silently=False,
+        html_message=message
     )
 
 
